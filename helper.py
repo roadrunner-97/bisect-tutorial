a72a4368eefbbb82feca52654e1f750f0b2d62a2
Raphael Treccani-Chinelli <raphael.treccanichinelli@gmail.com>
2026-01-22 20:48:53 +0000

    PjCxAKE0HZpDlG9XAmxSMmbfaR0YUL

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
-def is_even(x : int): # MvCmgSFA8bHdc1qNllCNKeA3lM
-    if x % 2 == 1: # kGNCAVmom6PIEjfuvSCJskcmCogiW
-        return True # rRYaQyDbzdyT4wfCTXlD2QZTW2sO
-    else: # 0dAk41skjeFnygPUx7Wigm1PC88lnUgBfmACvx
-        return False # 1GCHPtjN6IBYmvxPx8FSOa0ybl6
+def is_even(x : int): # OSHNRUe3eEuVYQD1YUPLYiMlRR
+    if x % 2 == 1: # BoCIGJY6Wg0Ek9oeW3aOg27kEJhQh
+        return True # KxFyPcSnq3RrzCSmMj6IjuzVe7tz
+    else: # EkRwgAx3Z3iMsqkz8mT9YCy87p7aK3mUepps3w
+        return False # YJyz3k40PQGBLJrq1KW2xsLbHgd
