dbd198b660156d04db847c99861c8490404ba9fd
Raphael Treccani-Chinelli <raphael.treccanichinelli@gmail.com>
2026-01-22 20:49:18 +0000

    UXxw3Q0yxO3SyuqXK0pzrwP1Evo

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
-def is_even(x : int): # VoSnkL6K6Fbv9Tg4uqTpnWiUb3
-    if x % 2 == 1: # EEPQQgghZOIyqwWjmM0DSs1YJPeaL
-        return True # aqYTqmCe89vAuVMgazHZZlKD1fdl
-    else: # E5evqyTrY7lduT8cn9f3k9A7eG8Nl22ag1ohf5
-        return False # OR1Obv0Fvqcsad1pokRNCudHy03
+def is_even(x : int): # LXQHfdDYlmhvjKh9hOhU7BK0Rx
+    if x % 2 == 1: # kLDmHUMY1rTYsOjL9etBsgruFk6VB
+        return True # ialQAYlNDgskalxmsUK0YPSvJcpR
+    else: # fTAnTMJyFPkB8EGegnbsD8A1j2V71njNIF2xhe
+        return False # pVwPf0wfsNMvBGMjiurMLZQEACy
